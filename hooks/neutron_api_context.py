9a9dc174ae21f7368251ff9dfbc117c330172a16
Salman Khan <salman.khan@yoti.com>
2019-08-28 14:42:29 +0000

    FWAAS v2 enabled

--- hooks/neutron_api_context.py
+++ hooks/neutron_api_context.py
@@ -540,12 +540,12 @@ class NeutronCCContext(context.NeutronContext):
                 'pike': ['router', 'firewall', 'metering',
                          ('neutron_lbaas.services.loadbalancer.plugin.'
                           'LoadBalancerPluginv2')],
-                'queens': ['router', 'firewall', 'metering', 'segments',
+                'queens': ['router', 'firewall_v2', 'metering', 'segments',
                            ('neutron_lbaas.services.loadbalancer.plugin.'
                             'LoadBalancerPluginv2'),
                            ('neutron_dynamic_routing.'
                             'services.bgp.bgp_plugin.BgpPlugin')],
-                'rocky': ['router', 'firewall', 'metering', 'segments',
+                'rocky': ['router', 'firewall_v2', 'metering', 'segments',
                           ('neutron_dynamic_routing.'
                            'services.bgp.bgp_plugin.BgpPlugin')],
             }
